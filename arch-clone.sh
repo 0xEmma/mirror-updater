f55446c9acf70babe7cb5fc45a232910f1cd1d56
Emma Ruby <emma@0xem.ma>
2023-06-05 10:42:54 -0400

    Update arch-clone.sh

--- arch-clone.sh
+++ arch-clone.sh
@@ -65,11 +65,8 @@ rsync_cmd() {
 	local -a cmd=(rsync -rlptH --safe-links --delete-delay --delay-updates
 		"--timeout=600" "--contimeout=60" --no-motd)
 
-	if stty &>/dev/null; then
-		cmd+=(-h -v --progress)
-	else
-		cmd+=(--quiet)
-	fi
+	cmd+=(-h -v --progress)
+
 
 	if ((bwlimit>0)); then
 		cmd+=("--bwlimit=$bwlimit")
